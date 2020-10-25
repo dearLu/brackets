f63fc139d4efd9ef34d5a513960b43818d5bcd00
dearLu <lady.barabanovapolina@list.ru>
2020-10-25 22:08:03 +0400

    feat: add method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let brackets = bracketsConfig;
+    let stack = []
+    let arr=[];
+    for(let i=0; i<brackets.length; i++){   
+      for(let j=0; j<brackets[i].length; j++){
+        arr.push(brackets[i][j]);
+      }
+  } 
+    for(let bracket of str) {
+      let bracketsIndex = arr.indexOf(bracket)
+  
+      if (bracketsIndex === -1){
+        continue
+      }
+  
+      if(bracketsIndex % 2 === 0) {
+        stack.push(bracketsIndex + 1)
+      } else {
+        if(stack.pop() !== bracketsIndex) {
+          return false;
+        }
+      }
+    }
+    return stack.length === 0
+  
 }
